0abbb4172a8fb5ad8450463c09e62ea7803c9ad7
Bjorn Lim <mail@bjornl.im>
2016-11-15 20:30:01 +0800

    Preliminary bus stuff on cache. Just need bus class and handle dragon after.

---
+++ src/BusOperation.java
@@ -0,0 +1,32 @@
+/**
+ * Stuff to do on the bus (~the wheels on the bus go round and round~).
+ */
+public class BusOperation implements Comparable<BusOperation>{
+
+    public Transaction transaction;
+    public Transaction lastTransaction;
+    public int address;
+    public int cacheCore;
+
+    public BusOperation(Transaction transaction, int address, int cacheCore) {
+        this.transaction = transaction;
+        this.address = address;
+        this.cacheCore = cacheCore;
+        this.lastTransaction = null;
+    }
+
+    public BusOperation(Transaction transaction, int address, int cacheCore, Transaction lastTransaction) {
+        this.transaction = transaction;
+        this.address = address;
+        this.cacheCore = cacheCore;
+        this.lastTransaction = lastTransaction;
+    }
+
+    @Override
+    public int compareTo(BusOperation operation) {
+        if (this.transaction == operation.transaction && this.cacheCore == operation.cacheCore)
+            return 1;
+        else
+            return 0;
+    }
+}
--- src/Cache.java
+++ src/Cache.java
@@ -45,6 +45,7 @@ public class Cache {
     private int associativity;
     private int blockSize;
     private Protocol protocol;
+    private int cacheCoreNumber; // the core which the cache is for
 
     private int arraySize;
     private int offsetBits;
@@ -52,24 +53,33 @@ public class Cache {
     private int tagBits;
 
     private int cacheAccesses;
+    private int memoryAccesses;
     private int readHit;
     private int readMiss;
     private int writeHit;
     private int writeMiss;
     private int currentCycle;
 
-    public Cache (int cacheSize, int associativity, int blockSize, Protocol proto) {
+    private boolean isStalled;
+    private BusOperation previousOperation;
+
+    public Cache (int cacheSize, int associativity, int blockSize, Protocol proto, int cacheCoreNumber) {
         cacheAccesses = 0;
+        memoryAccesses = 0;
         readHit = 0;
         readMiss = 0;
         writeHit = 0;
         writeMiss = 0;
         currentCycle = 0;
 
+        isStalled = false;
+        previousOperation = null;
+
         this.cacheSize = cacheSize;
         this.associativity = associativity;
         this.blockSize = blockSize;
         this.protocol = proto;
+        this.cacheCoreNumber = cacheCoreNumber;
 
         arraySize = cacheSize/blockSize/associativity;              // this is the number of "rows" in the cache
 
@@ -83,22 +93,24 @@ public class Cache {
         tagBits = 32 - offsetBits - indexBits;
 
         System.out.println("Protocol: " + this.protocol);
+        System.out.println("Core: " + this.cacheCoreNumber);
         System.out.println("Cache Size: " + this.cacheSize + " Bytes");
         System.out.println("Associativity: " + this.associativity);
         System.out.println("Block Size: " + this.blockSize + " Bytes");
         System.out.println("Offset: " + this.offsetBits + " Bits");
         System.out.println("Set Index: " + this.indexBits + " Bits");
         System.out.println("Tag bits: " + this.tagBits + " Bits");
+        System.out.println();
     }
 
-    public void readCache(int address) {
+    public void readCache (int address) {
         cacheAccesses += 1;
 
         int index = getIndex(address);
         FixedQueue<CacheBlock> row = l1cache.get(index);
         int tag = getTag(address);
 
-        for (int i = 0; i < associativity; i++) {
+        for (int i = 0; i < row.size(); i++) {
             if (tag == row.get(i).tag) {
                 // take care of the case of MSI/MESI and invalid state
                 if ((protocol == Protocol.MSI || protocol == Protocol.MESI) && row.get(i).state == State.INVALID) {
@@ -112,16 +124,24 @@ public class Cache {
         // from here, cache missed.
         readMiss += 1;
         // TODO: something on bus for readMiss
+        if (protocol == Protocol.MSI || protocol == Protocol.MESI) { // guranteed to be INVALID state
+            // bus function new BusOperation(Transaction.BUS_READ, cacheCoreNumber, address);
+            // change to SHARED_CLEAN state once data received
+        } else { //DRAGON
+            // TODO
+            //bus function new BusOperation(Transaction.BUS_READ_MISS, cacheCoreNumber, address);
+        }
+
     }
 
-    public void writeCache(int address) {
+    public void writeCache (int address) {
         cacheAccesses += 1;
 
         int index = getIndex(address);
         FixedQueue<CacheBlock> row = l1cache.get(index);
         int tag = getTag(address);
 
-        for (int i = 0; i < associativity; i++) {
+        for (int i = 0; i < row.size(); i++) {
             if (tag == row.get(i).tag) {
                 // take care of the case of MSI/MESI and invalid state
                 if ((protocol == Protocol.MSI || protocol == Protocol.MESI) && row.get(i).state == State.INVALID) {
@@ -129,6 +149,12 @@ public class Cache {
                 }
                 writeHit++;
                 // TODO: something on bus for writeHit
+                if ((protocol == Protocol.MSI || protocol == Protocol.MESI) && row.get(i).state == State.SHARED_CLEAN) {
+                    //bus function new BusOperation(Transaction.BUS_READ_EXCLUSIVE, cacheCoreNumber, address);
+                } else if (protocol == Protocol.DRAGON) {
+                    // TODO
+                    //do something for dragon Dx
+                }
                 reorderCache(row, i, tag);
                 return;
             }
@@ -136,25 +162,148 @@ public class Cache {
         // from here, cache missed.
         writeMiss += 1;
         // TODO: something on bus for writeMiss
+        if (protocol == Protocol.MSI || protocol == Protocol.MESI) { // guranteed to be INVALID state
+        } else { //DRAGON
+            // TODO
+        }
     }
 
-    public void busSnoop(int cycles) {
+    public void busSnoop (int cycles) {
         currentCycle = cycles;
-
+        // TODO: get operation from bus
+        // TODO: write bus class
+        if (operation = null) { // case where no operation
+            return;
+            // check if transaction is from current core and if transaction is completed
+        } else if (cacheCoreNumber == Bus.getCore() && Bus.isTransactionCompleted) {
+            if (operation.transaction == Transaction.BUS_FLUSH) {
+                // Bus.setReceived();
+                return;
+            }
+            if (Bus.hasTransactionResult) { //i.e, busRead/busReadX is successful
+                updateSelfCache(operation);
+                isStalled = false;
+                // Bus.setReceived();
+            } else {                        // i.e, busRead/busReadX is unsuccessful, read from main memory
+                memoryAccesses += 1;
+                isStalled = true;
+                // TODO: main memory access
+            }
+            // else this operation is from other cores, and we must update our state to reflect their operation
+        } else {
+            if (previousOperation.equals(operation)) {
+                return;
+            }
+            if (operation.transaction != Transaction.BUS_FLUSH) {
+                updateFromOtherCache(operation);
+            }
+            previousOperation = operation;
+        }
     }
 
-    private void msiProtocolBus(CacheBlock block, Transaction transaction) {
+    /**
+     * This method is called when there is an operation from the bus (from this core) that we need to execute that updates the cache
+     * @param operation
+     */
+    private void updateSelfCache (BusOperation operation) {
+        int index = getIndex(operation.address);
+        FixedQueue<CacheBlock> row = l1cache.get(index);
+        int tag = getTag(operation.address);
+        State state = null;
+        switch (operation.transaction) {
+            case BUS_READ:
+                if (protocol == Protocol.MSI || protocol == Protocol.MESI) {
+                    state = State.SHARED_CLEAN;
+                } else { //DRAGON
+                    // TODO
+                }
+                break;
+            case BUS_READ_EXCLUSIVE:
+                if (protocol == Protocol.MSI || protocol == Protocol.MESI) {
+                    state = State.SHARED_MODIFIED;
+                } else { //DRAGON
+                    // TODO
+                }
+                break;
+            case BUS_UPDATE: // DRAGON only
+                // TODO
+                break;
+            default:
+                break;
+        }
+        if (operation.lastTransaction == Transaction.BUS_READ && protocol == Protocol.MESI) {
+            state = State.EXCLUSIVE;
+        }
+        // TODO: DRAGON lastTransaction
+
+        if (state == null) {
+            System.out.println("updateSelfCache error in assigning state");
+            return;
+        }
+
+        for (int i = 0; i < associativity; i++) {
+            // case where the CacheSet queue is not full and no existing row to be updated (need to break loop early)
+            if (i == row.size() && i < associativity) {
+                row.add(new CacheBlock(state, tag));
+                break;
+            // case where CacheSet has an existing row to be updated
+            } else if (tag == row.get(i).tag) {
+                row.set(i, new CacheBlock(state, tag));
+                reorderCache(row, i, tag);
+                break;
+            // case where CacheSet queue is full and no existing row to be updated
+            } else {
+                row.add(new CacheBlock(state, tag));  // as it is a fixed size queue, will auto remove the LRU
+            }
+        }
+    }
+    /**
+     * This method is called when there is an operation from the bus (from OTHER core) that we need to execute that updates
+     * the cache. In this scenario, the block must exist in the cache before we can update.
+     * @param operation
+     */
+    private void updateFromOtherCache (BusOperation operation) {
+        int index = getIndex(operation.address);
+        FixedQueue<CacheBlock> row = l1cache.get(index);
+        int tag = getTag(operation.address);
+
+        for (int i = 0; i < row.size(); i++) {
+            if (tag == row.get(i).tag) {
+                CacheBlock block = row.get(i);
+                switch (protocol) {
+                    case MSI:
+                        msiProtocolBus(block, operation.transaction);
+                        break;
+                    case MESI:
+                        mesiProtocolBus(block, operation.transaction);
+                        break;
+                    case DRAGON:
+                        dragonProtocolBus(block, operation.transaction);
+                        break;
+                    default:
+                        break;
+                }
+                row.set(i, block);
+                reorderCache(row, i, tag);
+                break;
+            }
+        }
+    }
+
+    private void msiProtocolBus (CacheBlock block, Transaction transaction) {
         switch (block.state) {
             case MODIFIED:
-                if (transaction == Transaction.BUS_READ)
+                if (transaction == Transaction.BUS_READ) {
                     block.state = State.SHARED_CLEAN;
-                else if (transaction == Transaction.BUS_READ_EXCLUSIVE)
+                } else if (transaction == Transaction.BUS_READ_EXCLUSIVE) {
                     block.state = State.INVALID;
+                }
                 // TODO: flush data on bus
                 break;
             case SHARED_CLEAN:
-                if (transaction == Transaction.BUS_READ_EXCLUSIVE)
+                if (transaction == Transaction.BUS_READ_EXCLUSIVE) {
                     block.state = State.INVALID;
+                }
                 break;
             case INVALID:
                 break;
@@ -167,22 +316,25 @@ public class Cache {
     private void mesiProtocolBus (CacheBlock block, Transaction transaction) {
         switch (block.state) {
             case MODIFIED:
-                if (transaction == Transaction.BUS_READ)
+                if (transaction == Transaction.BUS_READ) {
                     block.state = State.SHARED_CLEAN;
-                else if (transaction == Transaction.BUS_READ_EXCLUSIVE)
+                } else if (transaction == Transaction.BUS_READ_EXCLUSIVE) {
                     block.state = State.INVALID;
+                }
                 // TODO: flush data on bus
                 break;
             case EXCLUSIVE:
-                if (transaction == Transaction.BUS_READ)
+                if (transaction == Transaction.BUS_READ) {
                     block.state = State.SHARED_CLEAN;
-                else if (transaction == Transaction.BUS_READ_EXCLUSIVE)
+                } else if (transaction == Transaction.BUS_READ_EXCLUSIVE) {
                     block.state = State.INVALID;
+                }
                 // TODO: flush data on bus
                 break;
             case SHARED_CLEAN:
-                if (transaction == Transaction.BUS_READ_EXCLUSIVE)
+                if (transaction == Transaction.BUS_READ_EXCLUSIVE) {
                     block.state = State.INVALID;
+                }
                 break;
             case INVALID:
                 break;
@@ -196,19 +348,22 @@ public class Cache {
     private void dragonProtocolBus (CacheBlock block, Transaction transaction) {
         switch (block.state) {
             case MODIFIED:
-                if (transaction == Transaction.BUS_READ)
+                if (transaction == Transaction.BUS_READ) {
                     block.state = State.SHARED_MODIFIED;
+                }
                 // TODO: flush data on bus
                 break;
             case EXCLUSIVE:
-                if (transaction == transaction.BUS_READ)
+                if (transaction == transaction.BUS_READ) {
                     block.state = State.SHARED_CLEAN;
+                }
                 break;
             case SHARED_CLEAN:
                 break;
             case SHARED_MODIFIED:
-                if (transaction == transaction.BUS_UPDATE)
+                if (transaction == transaction.BUS_UPDATE) {
                     block.state = State.SHARED_CLEAN;
+                }
                 // TODO: bus update???
                 break;
             default:
@@ -220,8 +375,9 @@ public class Cache {
     /**
      * quick and dirty way to get current stats
      */
-    public void printCacheStats() {
+    public void printCacheStats () {
         System.out.println("Number of times the cache was accessed: " + cacheAccesses);
+        System.out.println("Number of times main memory was accessed: " + memoryAccesses);
         System.out.println("Number of times there was a read hit: " + readHit);
         System.out.println("Number of times there was a read miss: " + readMiss);
         System.out.println("Number of times there was a write hit: " + writeHit);
@@ -246,7 +402,7 @@ public class Cache {
     private void reorderCache (FixedQueue<CacheBlock> row, int index, int tag) {
         State currentState = row.get(index).state;
         row.remove(index);
-        row.add(new CacheBlock(currentState,tag));
+        row.add(new CacheBlock(currentState, tag));
     }
 
     /**
